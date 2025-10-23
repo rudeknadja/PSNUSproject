a73f617adbe0bb9a71f5f88158e8db7771fbe036
rudeknadja <rudeknadja@gmail.com>
2025-10-23 14:26:09 +0200

    Gui

--- ScadaGUI/MainWindow.xaml.cs
+++ ScadaGUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +19,50 @@ namespace ScadaGUI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+        private DataConcentrator.DataConcentrator dc;
+        private ConfigurationManager configManager;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // 🟩 Inicijalizuj DataConcentrator i ConfigurationManager
+            dc = new DataConcentrator.DataConcentrator();
+            configManager = new ConfigurationManager();
+
+            // 🟩 (opciono) učitaj konfiguraciju
+            var loadedTags = configManager.LoadConfiguration();
+            foreach (var tag in loadedTags)
+            {
+                dc.AddTag(tag);
+            }
+
+            RefreshTables();
+        }
+
+        private void RefreshTables()
+        {
+            List<Tag> tags = dc.GetAllTags();
+            List<Alarm> alarms = dc.GetActiveAlarms();
+
+            TagDataGrid.ItemsSource = null;
+            TagDataGrid.ItemsSource = tags;
+
+            AlarmDataGrid.ItemsSource = null;
+            AlarmDataGrid.ItemsSource = alarms;
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshTables();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
